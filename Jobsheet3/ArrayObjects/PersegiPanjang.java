2729f1c9fdaeb495dc56fb58536309a65b9c9f28
User-425 <user425.itsme@gmail.com>
2024-03-02 19:21:30 +0700

    add contructors

--- Jobsheet3/ArrayObjects/PersegiPanjang.java
+++ Jobsheet3/ArrayObjects/PersegiPanjang.java
@@ -2,4 +2,16 @@ package ArrayObjects;
 public class PersegiPanjang {
     public int panjang;
     public int lebar;
+
+    public PersegiPanjang(){
+        
+    }
+    public PersegiPanjang(int p, int l){
+        this(p);
+        this.lebar = l;
+    }
+    public PersegiPanjang(int p){
+        this();
+        this.panjang = p;
+    }
 }
