9483bb6e61915c6fa81e205a059de1aaa8e95d69
User-425 <user425.itsme@gmail.com>
2024-03-22 10:33:35 +0700

    Commit

---
+++ Jobsheet4/BruteForceDivideConquer/Minggu 5/Faktorial.java
@@ -0,0 +1,28 @@
+/**
+ * Faktorial
+ */
+public class Faktorial {
+
+    public int nilai;
+
+    public int faktorialBF(int n){
+        int fakto = 1;
+        int i = 1;
+        while (i <= n) { // Jawaban no 2
+            fakto = fakto * i;
+            i++;
+        }
+        return fakto;
+    }
+    
+
+    public int faktorialDC(int n){
+        if (n == 1) {
+            return 1;
+        }else{
+            int fakto = n * faktorialDC(n - 1);
+            return fakto;
+        }
+    }
+   
+}   
